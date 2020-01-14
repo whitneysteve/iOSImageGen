20f9d81165507c0d0470dc941172a01ee91b26d7
Stephen Whitney <steve.whitney@gmail.com>
2020-01-14 22:13:38 +0000

    Switch background to name by dimension

--- background_resize.rb
+++ background_resize.rb
@@ -7,47 +7,38 @@ if __FILE__ == $0
   input_file_name = ARGV.shift
   output_dir = ARGV.shift
 
-  resses = {
-    'ios89_iphone_portrait_hd_55' => [1242, 2208],
-    'ios89_iphone_portrait_hd_47' => [750, 1334],
-    'ios89_iphone_landscape_hd_55' => [2208, 1242],
-    'ios8_iphone_retina_hd_5.5_landscape' => [2208, 1242],
-    'ios8_iphone_retina_hd_5.5_portrait' => [1242, 2208],
-    'ios8_iphone_retina_hd_4.7_portrait' => [750, 1334],
-    'ios78_iphone_retina_4_portrait' => [640, 1136],
-    'ios78_iphone_2x_portrait' => [640, 960],
-    'ios78_ipad_2x_landscape' => [2048, 1536],
-    'ios78_ipad_1x_landscape' => [1024, 768],
-    'ios78_ipad_2x_portrait' => [1536, 2048],
-    'ios78_ipad_1x_portrait' => [768, 1024],
-    'ios56_iphone_retina_4_portrait' => [640, 1136],
-    'ios56_iphone_2x_portrait' => [640, 960],
-    'ios56_iphone_1x_portrait' => [320, 480],
-    'ios56_ipad_2x_landscape' => [2048, 1536],
-    'ios56_ipad_1x_landscape' => [1024, 768],
-    'ios56_ipad_2x_landscape_no_status' => [2048, 1496],
-    'ios56_ipad_1x_landscape_no_status' => [1024, 748],
-    'ios56_ipad_2x_portrait' => [1536, 2048],
-    'ios56_ipad_1x_portrait' => [768, 1024],
-    'ios56_ipad_2x_portrait_no_status' => [1536, 2008],
-    'ios56_ipad_1x_portrait_no_status' => [768, 1004],
-  }
+  resses = [
+    [320, 480],
+    [640, 960],
+    [640, 1136],
+    [750, 1334],
+    [768, 1004],
+    [768, 1024],
+    [828, 1792],
+    [1024, 748],
+    [1024, 768],
+    [1125, 2436],
+    [1242, 2208],
+    [1242, 2688],
+    [1536, 2008],
+    [1536, 2048],
+    [1792, 828],
+    [2048, 1496],
+    [2048, 1536],
+    [2208, 1242],
+    [2436, 1125],
+    [2688, 1242],
+  ]
 
-  resses.keys.each { |res_key|
-    puts "Generating #{res_key}"
-    res = resses[res_key]
+  resses.each { |res|
     width = res[0]
     height = res[1]
-    largest = if width > height
-      width
-    else
-      height
-    end
+    res_key = "#{width}x#{height}"
+    puts "Generating #{res_key}"
+
     image = Magick::Image.read(input_file_name).first
-    image.change_geometry!("#{largest}x#{largest}") { |cols, rows, img|
-      newimg = img.resize(cols, rows)
-      newimg.crop(0, 0, width, height).write("#{output_dir}/#{res_key}.png")
-    }
+    newimg = image.resize_to_fill(width, height)
+    newimg.write("#{output_dir}/#{res_key}.png")
   }
 
 end
